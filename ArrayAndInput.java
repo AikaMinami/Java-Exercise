e2c00976bed74c2c5d63d89afdcf9acecdb14723
Aikacchii <45847485+AikaMinami@users.noreply.github.com>
2021-02-05 10:40:35 +0700

    Update ArrayAndInput.java

--- ArrayAndInput.java
+++ ArrayAndInput.java
@@ -3,9 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Example;
 
-//import Scanner 
+//import library untuk Scanner 
 import java.util.Scanner;
 /**
  *
