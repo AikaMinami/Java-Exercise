db0c6877c45e1e4de032e0e4e565e9b150f81210
Aikacchii <45847485+AikaMinami@users.noreply.github.com>
2021-02-05 10:24:05 +0700

    Add Exercise
    Problem 1

---
+++ Problem1.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Problem_1;
+
+/**
+ *
+ * @author Aikacchii
+ */
+public class Problem1 {
+    // Di problem ini akan ada persoalan mengenai :
+    // Array, fungsi, pengulangan(rekursi), if-else(kondisi)
+    
+    // 1. Mulai dengan membuat fungsi main terlebih dahulu!
+    
+    // 2. Isi fungsi main tersebut dengan membuat array bernama "arr"
+    //      yang memiliki panjang 5 dan bertipe data integer
+    
+    // 3. Lalu buatlah Scanner agar user bisa menginput angka di array tersebut
+    
+    // 4. Buat inputan untuk user dengan menggunakan nextInt
+    
+    
+    // 5. Buat fungsi yang memiliki tipe data void dengan nama "Operation" 
+    //      dengan parameter array bertipe data integer
+    
+    // 6. Di dalam fungsi tersebut, isilah dengan variabel bertipe data integer,
+    //    nama "result", dan nilainya 0
+    
+    
+    // 7. Lalu buat rekursi sebanyak panjang array
+    
+
+    // 8. Isi rekursi tersebut dengan kondisi sebagai berikut :
+    //    a. Jika nilai dari array di index yang sedang dicek adalah ganjil,
+    //       maka nilai dari array di index tersebut
+    //       "dijumlahkan" dengan variabel "result" 
+    //    b. Jika nilai dari array di index yang sedang dicek adalah genap,
+    //       maka nilai dari array di index tersebut
+    //       "dikurangi" dengan variabel "result" 
+    
+    
+    // 9. Pada fungsi main, panggil fungsi "Operation" dengan parameter array "arr"
+    
+}
+
+// Hint :
+// 1.1. Fungsi main bisa dibuat di Apache NetBeans dengan menggunakan 
+//      mengetik main lalu klik tab
+
+// 6.1. Apabila variabel integer akan dioperasikan namun tidak memiliki nilai awal,
+//      akan timbul error yang bertuliskan variabel belum dideklarasi
+
+// 8.1. Kalian bisa menggunakan operator assignment 
